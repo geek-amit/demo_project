ae04e04cbb7afde7bfe6f1c53365db5723aa5dad
geek-amit <geek.amit00@gmail.com>
2019-08-21 01:10:30 +0530

    first commit

---
+++ Hello world.cpp
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<conio.h>
+voidmain()
+{
+clrscr(
+);
+rintf("Hello world");
+getch();
+}
---
+++ if_else_statemnt.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=10;
+	if(a>9)
+	{
+	 printf("The number is greater than 9");	
+	}
+	else
+	{
+     printf("The number is less than 9");
+	}
+	getch();
+}
